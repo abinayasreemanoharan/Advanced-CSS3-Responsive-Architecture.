9b41284447605b84da65c5754206d2b3130d64c4
ABINAYA SREE M <abinayasree.m_25it@kgkite.ac.in>
2026-07-26 23:21:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("themeBtn");
+
+btn.onclick = () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+btn.innerHTML="☀️";
+
+}
+else{
+
+btn.innerHTML="🌙";
+
+}
+
+};
